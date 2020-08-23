c4056fa63817f88419c87154eb34191248112cdc
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-08-23 22:02:13 +0530

    view doc

--- app/src/main/java/com/ddvader44/officebuddy/doc/docActivity.java
+++ app/src/main/java/com/ddvader44/officebuddy/doc/docActivity.java
@@ -3,22 +3,34 @@ package com.ddvader44.officebuddy.doc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.ddvader44.officebuddy.BuildConfig;
 import com.ddvader44.officebuddy.R;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class docActivity extends AppCompatActivity {
     ListView docListView;
@@ -39,13 +51,25 @@ public class docActivity extends AppCompatActivity {
         docListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(docActivity.this, viewDocActivity.class);
-                intent.putExtra("position", position);
+                String name = parent.getItemAtPosition(position).toString();
+                String fileUrl = name;
+                File file = new File(fileUrl);
+                String mime = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+
+               Uri theUri =  FileProvider.getUriForFile(Objects.requireNonNull(getApplicationContext()),
+                        BuildConfig.APPLICATION_ID + ".provider", file);
+
+                Intent intent = new Intent();
+
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                intent.setDataAndType(theUri, mime);
+
                 startActivity(intent);
 
             }
         });
-    }
+            }
 
     private void missionPermission() {
         if ((ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
@@ -81,7 +105,7 @@ public class docActivity extends AppCompatActivity {
                 } else {
 
                     boolean booleanpdf = false;
-                    if (listFile[i].getName().endsWith(".docx")) {
+                    if (listFile[i].getName().endsWith(".docx") || listFile[i].getName().endsWith(".doc")) {
 
                         for (int j = 0; j < fileList.size(); j++) {
                             if (fileList.get(j).getName().equals(listFile[i].getName())) {
